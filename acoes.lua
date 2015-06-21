a3846ee4f0290d7f3e34fdbbba0eb13a5662f21a
Felipe Borges <jfpsb_felipe2@hotmail.com>
2015-06-21 20:15:21 -0300

    Revertido

--- acoes.lua
+++ acoes.lua
@@ -43,7 +43,6 @@ acoes = {
 					local tiro = {}
 					tiro.x = nave.x + (naveImg:getWidth()/2) - (balaImg:getWidth()/2)
 					tiro.y = nave.y
-					tiro.som = tiroSound
 					table.insert(nave.tiros, tiro)
 					shotsFired = shotsFired + 1
 
